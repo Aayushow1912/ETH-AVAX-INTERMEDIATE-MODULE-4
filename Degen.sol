8a92eaa283da97a89a85f78f01dc998e9cb47510
Aayush Tewari <113117171+Aayushow1912@users.noreply.github.com>
2024-09-30 11:32:00 +0530

    Update Degen.sol

--- Degen.sol
+++ Degen.sol
@@ -7,77 +7,113 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    event TokensRedeemed(address indexed redeemer, uint256 amount, string item);
-
     struct Item {
         string name;
+        uint8 itemId;
         uint256 price;
-        uint256 stock;
+    }
+    mapping (uint8=>Item) items;
+    mapping (address=>Item[]) playerItems;
+    uint8 public tokenId;
+    
+    event ItemPurchased(address indexed buyer, uint8 itemId, string itemName, uint256 price);
+    event GameOutcome(address indexed player, uint256 num, bool won, string result);
+
+    constructor (address initialOwner, uint tokenSupply) ERC20("Degen", "DGN") Ownable(initialOwner) {
+        mint(initialOwner, tokenSupply);
+        
+        items[1] = Item("Beginner's Beacon", 1, 100);
+        items[2] = Item("Epic Explorer", 2, 700);
+        items[3] = Item("Galactic Guardian", 3, 1200);
+        items[4] = Item("Stellar Sentinel", 4, 2200);
+        items[5] = Item("Ultimate Overlord", 5, 2400);
+        tokenId = 6;
+
     }
 
-    Item[] public items;
-
-    mapping(uint256 => uint256) public itemStock;
-    mapping(address => mapping(uint256 => uint256)) public userInventory;
-
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        _initializeItems();
+    function decimals() override public pure returns (uint8){
+        return 0;
     }
 
-    function _initializeItems() internal {
-       items[1] = Item("Beginner's Beacon", 1, 100);
-       items[2] = Item("Epic Explorer", 2, 700);
-       items[3] = Item("Galactic Guardian", 3, 1200);
-       items[4] = Item("Stellar Sentinel", 4, 2200);
-       items[5] = Item("Ultimate Overlord", 5, 2400);
-       tokenId = 6;
-
-        // Initialize item stock mapping
-        for (uint256 i = 0; i < items.length; i++) {
-            itemStock[i] = items[i].stock;
-        }
-    }
+    // Minting tokens
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function getBalance() external view returns (uint256) {
+    // Transferring tokens
+
+    function transferToken(address _recipient, uint _amount) external {
+        require(balanceOf(msg.sender)>=_amount, "Insufficient balance");
+        transfer(_recipient, _amount);
+    }
+
+    // Redeeming tokens
+
+    function welcomeBonus() public {
+        require(balanceOf(msg.sender) == 0, "You've already claimed your welcome bonus");
+        _mint(msg.sender, 50);
+    }
+
+    function addItem(string memory _name, uint256 _price) public onlyOwner {
+        items[tokenId] = Item(_name, tokenId,_price);
+        tokenId++;
+    } 
+
+    function isLessThanFive(bool _prediction, uint256 _betAmount) public {
+        uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 10;
+
+        if (_prediction ==(randomNumber<5)) {
+            _mint(msg.sender, _betAmount*2);
+            emit GameOutcome(msg.sender, randomNumber, true, "won");
+
+        } else {
+            burn(_betAmount);
+            emit GameOutcome(msg.sender, randomNumber, false, "lost");
+        }
+    }
+    
+    function purchaseItem(uint8 _itemId) external {
+        require(items[_itemId].price != 0, "Item not found");
+        require(balanceOf(msg.sender) >= items[_itemId].price, "Insufficient balance");
+
+        burn(items[_itemId].price);
+        playerItems[msg.sender].push(items[_itemId]);
+
+        emit ItemPurchased(msg.sender, _itemId, items[_itemId].name, items[_itemId].price);
+    }
+
+    function getUserItems(address user) external view returns (uint8[] memory) {
+        Item[] memory itemsList = playerItems[user];
+        uint length = itemsList.length;
+
+        uint8[] memory _ids = new uint8[](length);
+
+        for (uint i = 0; i < length; i++) {
+            _ids[i] = itemsList[i].itemId;
+        }
+
+        return _ids;
+    }
+    function getItemName(uint8 _id) external  view returns (string memory) {
+        return items[_id].name;
+    }
+    function getItemPrice(uint8 _id) external  view returns(uint){
+        return items[_id].price;
+    }
+
+
+    // Checking token balance
+
+    function getBalance() external view returns(uint256){
         return balanceOf(msg.sender);
     }
 
-    function transferTokens(address _receiver, uint256 _value) external {
-        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
-        transfer(_receiver, _value);
+    // Burning tokens
+
+    function burnToken(uint _amount) external {
+        require(balanceOf(msg.sender)>=_amount, "Insufficient amount");
+        burn(_amount);
     }
 
-    function burnTokens(uint256 _value) external {
-        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
-        burn(_value);
-    }
-
-    function redeemTokens(uint256 _choice) external {
-        require(_choice > 0 && _choice <= items.length, "Invalid choice");
-        Item storage item = items[_choice - 1];
-        require(itemStock[_choice - 1] > 0, "Item out of stock");
-        require(balanceOf(msg.sender) >= item.price, "You do not have enough Degen Tokens to redeem this item");
-
-        _burn(msg.sender, item.price);
-        itemStock[_choice - 1]--;
-        userInventory[msg.sender][_choice - 1]++;
-        
-        emit TokensRedeemed(msg.sender, item.price, item.name);
-    }
-
-    function storeItems() external view returns (Item[] memory) {
-        return items;
-    }
-
-    function getUserInventory(address user) external view returns (uint256[] memory) {
-        uint256[] memory inventory = new uint256[](items.length);
-        for (uint256 i = 0; i < items.length; i++) {
-            inventory[i] = userInventory[user][i];
-        }
-        return inventory;
-    }
 }
